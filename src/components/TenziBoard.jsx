c6318a5ed94ef7c0cdc334776c193125b03b95e7
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-02-22 13:52:30 +0000

    Add dice to board

--- src/components/TenziBoard.jsx
+++ src/components/TenziBoard.jsx
@@ -1,11 +1,46 @@
 const TenziBoard = () => {
+
+
   return (
       <div className="board">
           <div className="play-board">
-              <h2>Tenzies</h2>
+              <h2 className="title">Tenzies</h2>
               <p>
                 Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
               </p>
+            <div className="dice-container">
+                <span className="dice">
+                  6
+                </span>
+                <span className="dice green">
+                  2
+                </span>
+                <span className="dice">
+                  5
+                </span>
+                <span className="dice">
+                  2
+                </span>
+                <span className="dice">
+                  2
+                </span>
+                <span className="dice">
+                  2
+                </span>
+                <span className="dice">
+                  3
+                </span>
+                <span className="dice">
+                  4
+                </span>
+                <span className="dice green">
+                  2
+                </span>
+                <span className="dice">
+                  1
+                </span>
+            </div>
+            <button className="button">Roll</button>
           </div>
       </div>
   )
