d83fa02ffee81cce2ae5f407d5d687393b073fb1
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-02-22 13:13:50 +0000

    Setup tenzies board

---
+++ src/components/TenziBoard.jsx
@@ -0,0 +1,14 @@
+const TenziBoard = () => {
+  return (
+      <div className="board">
+          <div className="play-board">
+              <h2>Tenzies</h2>
+              <p>
+                Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+              </p>
+          </div>
+      </div>
+  )
+}
+
+export default TenziBoard;
