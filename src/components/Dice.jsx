c31934c955e59011ea2669ef46c1983949037674
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-02-23 01:37:28 +0000

    Move dice into its own component

---
+++ src/components/Dice.jsx
@@ -0,0 +1,40 @@
+const Dice = () => {
+  return (
+    <>
+      <div className="dice-container">
+        <span className="dice">
+          6
+        </span>
+        <span className="dice green">
+          2
+        </span>
+        <span className="dice">
+          5
+        </span>
+        <span className="dice">
+          2
+        </span>
+        <span className="dice">
+          2
+        </span>
+        <span className="dice">
+          2
+        </span>
+        <span className="dice">
+          3
+        </span>
+        <span className="dice">
+          4
+        </span>
+        <span className="dice green">
+          2
+        </span>
+        <span className="dice">
+          1
+        </span>
+      </div>
+    </>
+  )
+}
+
+export default Dice;
--- src/components/TenziBoard.jsx
+++ src/components/TenziBoard.jsx
@@ -1,3 +1,5 @@
+import Dice from "./Dice";
+
 const TenziBoard = () => {
 
 
@@ -8,38 +10,7 @@ const TenziBoard = () => {
               <p>
                 Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
               </p>
-            <div className="dice-container">
-                <span className="dice">
-                  6
-                </span>
-                <span className="dice green">
-                  2
-                </span>
-                <span className="dice">
-                  5
-                </span>
-                <span className="dice">
-                  2
-                </span>
-                <span className="dice">
-                  2
-                </span>
-                <span className="dice">
-                  2
-                </span>
-                <span className="dice">
-                  3
-                </span>
-                <span className="dice">
-                  4
-                </span>
-                <span className="dice green">
-                  2
-                </span>
-                <span className="dice">
-                  1
-                </span>
-            </div>
+              <Dice />
             <button className="button">Roll</button>
           </div>
       </div>
